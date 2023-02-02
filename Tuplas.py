1f1099750a0c9defc0148b97fe8b9ec88fa1ba9e
Guilherme R Galanti <galanti_imp@hotmail.com>
2023-02-02 16:17:34 -0300

    fim

---
+++ Tuplas.py
@@ -0,0 +1,10 @@
+# Parênteses é utilizado para precedência em operações matemáticas, por isso usa-se a vírgula
+# Tuplas são imutáveis, por isso tem valorna programação
+
+frutas = ("larnja", "maça", "uva",)
+
+matriz = (
+    (1, 2, 3),
+    ("B", 3, "F"),
+    (6, 3, "C")
+)
